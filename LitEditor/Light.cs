5ec0ac02475f49cfe761f0ba25940711f457f20c
devast-PC\devast <devast3d@gmail.com>
2016-10-14 00:28:24 +0300

    wip

---
+++ LitEditor/Light.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LitEditor
+{
+    struct Vector3
+    {
+        public int X;
+        public int Y;
+        public int Z;
+    }
+
+    class Light
+    {
+        public bool Enabled;
+        public Vector3 Position;
+        public int Unknown1;
+        public int Unknown2;
+        public Vector3 Color;
+    }
+}
