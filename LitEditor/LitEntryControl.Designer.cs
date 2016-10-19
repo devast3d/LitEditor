a23b88de60cb5cbb9ed817883d9c9309d2ec75d6
devast3d <devast3d@gmail.com>
2016-10-19 21:34:16 +0300

    reworking light control

--- LitEditor/LitEntryControl.Designer.cs
+++ LitEditor/LitEntryControl.Designer.cs
@@ -28,156 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this._position_label = new System.Windows.Forms.Label();
-            this._positionX_textBox = new System.Windows.Forms.TextBox();
-            this._unknown_label = new System.Windows.Forms.Label();
-            this._unknown1_textBox = new System.Windows.Forms.TextBox();
-            this._unknown2_textBox = new System.Windows.Forms.TextBox();
-            this._colorR_textBox = new System.Windows.Forms.TextBox();
-            this._color_label = new System.Windows.Forms.Label();
-            this._positionY_textBox = new System.Windows.Forms.TextBox();
-            this._positionZ_textBox = new System.Windows.Forms.TextBox();
-            this._colorG_textBox = new System.Windows.Forms.TextBox();
-            this._colorB_textBox = new System.Windows.Forms.TextBox();
-            this._enabled_checkBox = new System.Windows.Forms.CheckBox();
-            this._enabled_label = new System.Windows.Forms.Label();
-            this._remove_button = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // _position_label
-            // 
-            this._position_label.AutoSize = true;
-            this._position_label.Location = new System.Drawing.Point(3, 32);
-            this._position_label.Name = "_position_label";
-            this._position_label.Size = new System.Drawing.Size(44, 13);
-            this._position_label.TabIndex = 0;
-            this._position_label.Text = "Position";
-            // 
-            // _positionX_textBox
-            // 
-            this._positionX_textBox.Location = new System.Drawing.Point(62, 29);
-            this._positionX_textBox.Name = "_positionX_textBox";
-            this._positionX_textBox.Size = new System.Drawing.Size(65, 20);
-            this._positionX_textBox.TabIndex = 1;
-            // 
-            // _unknown_label
-            // 
-            this._unknown_label.AutoSize = true;
-            this._unknown_label.Location = new System.Drawing.Point(3, 58);
-            this._unknown_label.Name = "_unknown_label";
-            this._unknown_label.Size = new System.Drawing.Size(53, 13);
-            this._unknown_label.TabIndex = 2;
-            this._unknown_label.Text = "Unknown";
-            // 
-            // _unknown1_textBox
-            // 
-            this._unknown1_textBox.Location = new System.Drawing.Point(62, 55);
-            this._unknown1_textBox.Name = "_unknown1_textBox";
-            this._unknown1_textBox.Size = new System.Drawing.Size(65, 20);
-            this._unknown1_textBox.TabIndex = 4;
-            // 
-            // _unknown2_textBox
-            // 
-            this._unknown2_textBox.Location = new System.Drawing.Point(133, 55);
-            this._unknown2_textBox.Name = "_unknown2_textBox";
-            this._unknown2_textBox.Size = new System.Drawing.Size(65, 20);
-            this._unknown2_textBox.TabIndex = 5;
-            // 
-            // _colorR_textBox
-            // 
-            this._colorR_textBox.Location = new System.Drawing.Point(62, 81);
-            this._colorR_textBox.Name = "_colorR_textBox";
-            this._colorR_textBox.Size = new System.Drawing.Size(65, 20);
-            this._colorR_textBox.TabIndex = 6;
-            // 
-            // _color_label
-            // 
-            this._color_label.AutoSize = true;
-            this._color_label.Location = new System.Drawing.Point(3, 84);
-            this._color_label.Name = "_color_label";
-            this._color_label.Size = new System.Drawing.Size(31, 13);
-            this._color_label.TabIndex = 7;
-            this._color_label.Text = "Color";
-            // 
-            // _positionY_textBox
-            // 
-            this._positionY_textBox.Location = new System.Drawing.Point(133, 29);
-            this._positionY_textBox.Name = "_positionY_textBox";
-            this._positionY_textBox.Size = new System.Drawing.Size(65, 20);
-            this._positionY_textBox.TabIndex = 8;
-            // 
-            // _positionZ_textBox
-            // 
-            this._positionZ_textBox.Location = new System.Drawing.Point(204, 29);
-            this._positionZ_textBox.Name = "_positionZ_textBox";
-            this._positionZ_textBox.Size = new System.Drawing.Size(65, 20);
-            this._positionZ_textBox.TabIndex = 9;
-            // 
-            // _colorG_textBox
-            // 
-            this._colorG_textBox.Location = new System.Drawing.Point(133, 81);
-            this._colorG_textBox.Name = "_colorG_textBox";
-            this._colorG_textBox.Size = new System.Drawing.Size(65, 20);
-            this._colorG_textBox.TabIndex = 10;
-            // 
-            // _colorB_textBox
-            // 
-            this._colorB_textBox.Location = new System.Drawing.Point(204, 81);
-            this._colorB_textBox.Name = "_colorB_textBox";
-            this._colorB_textBox.Size = new System.Drawing.Size(65, 20);
-            this._colorB_textBox.TabIndex = 11;
-            // 
-            // _enabled_checkBox
-            // 
-            this._enabled_checkBox.AutoSize = true;
-            this._enabled_checkBox.Location = new System.Drawing.Point(62, 5);
-            this._enabled_checkBox.Name = "_enabled_checkBox";
-            this._enabled_checkBox.Size = new System.Drawing.Size(15, 14);
-            this._enabled_checkBox.TabIndex = 13;
-            this._enabled_checkBox.UseVisualStyleBackColor = true;
-            // 
-            // _enabled_label
-            // 
-            this._enabled_label.AutoSize = true;
-            this._enabled_label.Location = new System.Drawing.Point(3, 6);
-            this._enabled_label.Name = "_enabled_label";
-            this._enabled_label.Size = new System.Drawing.Size(46, 13);
-            this._enabled_label.TabIndex = 14;
-            this._enabled_label.Text = "Enabled";
-            // 
-            // _remove_button
-            // 
-            this._remove_button.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this._remove_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this._remove_button.Location = new System.Drawing.Point(249, 2);
-            this._remove_button.Name = "_remove_button";
-            this._remove_button.Size = new System.Drawing.Size(20, 20);
-            this._remove_button.TabIndex = 15;
-            this._remove_button.Text = "x";
-            this._remove_button.UseVisualStyleBackColor = true;
-            // 
-            // LitEntryControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this._remove_button);
-            this.Controls.Add(this._enabled_label);
-            this.Controls.Add(this._enabled_checkBox);
-            this.Controls.Add(this._colorB_textBox);
-            this.Controls.Add(this._colorG_textBox);
-            this.Controls.Add(this._positionZ_textBox);
-            this.Controls.Add(this._positionY_textBox);
-            this.Controls.Add(this._color_label);
-            this.Controls.Add(this._colorR_textBox);
-            this.Controls.Add(this._unknown2_textBox);
-            this.Controls.Add(this._unknown1_textBox);
-            this.Controls.Add(this._unknown_label);
-            this.Controls.Add(this._positionX_textBox);
-            this.Controls.Add(this._position_label);
-            this.Name = "LitEntryControl";
-            this.Size = new System.Drawing.Size(279, 110);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this._position_label = new System.Windows.Forms.Label();
+			this._positionX_textBox = new System.Windows.Forms.TextBox();
+			this._flags_label = new System.Windows.Forms.Label();
+			this._unknown2_textBox = new System.Windows.Forms.TextBox();
+			this._colorR_textBox = new System.Windows.Forms.TextBox();
+			this._color_label = new System.Windows.Forms.Label();
+			this._positionY_textBox = new System.Windows.Forms.TextBox();
+			this._positionZ_textBox = new System.Windows.Forms.TextBox();
+			this._colorG_textBox = new System.Windows.Forms.TextBox();
+			this._colorB_textBox = new System.Windows.Forms.TextBox();
+			this._enabled_checkBox = new System.Windows.Forms.CheckBox();
+			this._enabled_label = new System.Windows.Forms.Label();
+			this._remove_button = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.checkBox5 = new System.Windows.Forms.CheckBox();
+			this.checkBox6 = new System.Windows.Forms.CheckBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox7 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// _position_label
+			// 
+			this._position_label.AutoSize = true;
+			this._position_label.Location = new System.Drawing.Point(3, 32);
+			this._position_label.Name = "_position_label";
+			this._position_label.Size = new System.Drawing.Size(44, 13);
+			this._position_label.TabIndex = 0;
+			this._position_label.Text = "Position";
+			// 
+			// _positionX_textBox
+			// 
+			this._positionX_textBox.Location = new System.Drawing.Point(62, 29);
+			this._positionX_textBox.Name = "_positionX_textBox";
+			this._positionX_textBox.Size = new System.Drawing.Size(65, 20);
+			this._positionX_textBox.TabIndex = 1;
+			// 
+			// _flags_label
+			// 
+			this._flags_label.AutoSize = true;
+			this._flags_label.Location = new System.Drawing.Point(3, 58);
+			this._flags_label.Name = "_flags_label";
+			this._flags_label.Size = new System.Drawing.Size(32, 13);
+			this._flags_label.TabIndex = 2;
+			this._flags_label.Text = "Flags";
+			// 
+			// _unknown2_textBox
+			// 
+			this._unknown2_textBox.Location = new System.Drawing.Point(62, 133);
+			this._unknown2_textBox.Name = "_unknown2_textBox";
+			this._unknown2_textBox.Size = new System.Drawing.Size(65, 20);
+			this._unknown2_textBox.TabIndex = 5;
+			// 
+			// _colorR_textBox
+			// 
+			this._colorR_textBox.Location = new System.Drawing.Point(62, 159);
+			this._colorR_textBox.Name = "_colorR_textBox";
+			this._colorR_textBox.Size = new System.Drawing.Size(65, 20);
+			this._colorR_textBox.TabIndex = 6;
+			// 
+			// _color_label
+			// 
+			this._color_label.AutoSize = true;
+			this._color_label.Location = new System.Drawing.Point(3, 162);
+			this._color_label.Name = "_color_label";
+			this._color_label.Size = new System.Drawing.Size(31, 13);
+			this._color_label.TabIndex = 7;
+			this._color_label.Text = "Color";
+			// 
+			// _positionY_textBox
+			// 
+			this._positionY_textBox.Location = new System.Drawing.Point(133, 29);
+			this._positionY_textBox.Name = "_positionY_textBox";
+			this._positionY_textBox.Size = new System.Drawing.Size(65, 20);
+			this._positionY_textBox.TabIndex = 8;
+			// 
+			// _positionZ_textBox
+			// 
+			this._positionZ_textBox.Location = new System.Drawing.Point(204, 29);
+			this._positionZ_textBox.Name = "_positionZ_textBox";
+			this._positionZ_textBox.Size = new System.Drawing.Size(65, 20);
+			this._positionZ_textBox.TabIndex = 9;
+			// 
+			// _colorG_textBox
+			// 
+			this._colorG_textBox.Location = new System.Drawing.Point(133, 159);
+			this._colorG_textBox.Name = "_colorG_textBox";
+			this._colorG_textBox.Size = new System.Drawing.Size(65, 20);
+			this._colorG_textBox.TabIndex = 10;
+			// 
+			// _colorB_textBox
+			// 
+			this._colorB_textBox.Location = new System.Drawing.Point(204, 159);
+			this._colorB_textBox.Name = "_colorB_textBox";
+			this._colorB_textBox.Size = new System.Drawing.Size(65, 20);
+			this._colorB_textBox.TabIndex = 11;
+			// 
+			// _enabled_checkBox
+			// 
+			this._enabled_checkBox.AutoSize = true;
+			this._enabled_checkBox.Location = new System.Drawing.Point(62, 5);
+			this._enabled_checkBox.Name = "_enabled_checkBox";
+			this._enabled_checkBox.Size = new System.Drawing.Size(15, 14);
+			this._enabled_checkBox.TabIndex = 13;
+			this._enabled_checkBox.UseVisualStyleBackColor = true;
+			// 
+			// _enabled_label
+			// 
+			this._enabled_label.AutoSize = true;
+			this._enabled_label.Location = new System.Drawing.Point(3, 6);
+			this._enabled_label.Name = "_enabled_label";
+			this._enabled_label.Size = new System.Drawing.Size(46, 13);
+			this._enabled_label.TabIndex = 14;
+			this._enabled_label.Text = "Enabled";
+			// 
+			// _remove_button
+			// 
+			this._remove_button.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this._remove_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this._remove_button.Location = new System.Drawing.Point(249, 2);
+			this._remove_button.Name = "_remove_button";
+			this._remove_button.Size = new System.Drawing.Size(20, 20);
+			this._remove_button.TabIndex = 15;
+			this._remove_button.Text = "x";
+			this._remove_button.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(62, 57);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(65, 17);
+			this.checkBox1.TabIndex = 16;
+			this.checkBox1.Text = "Shadow";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(62, 83);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(49, 17);
+			this.checkBox2.TabIndex = 17;
+			this.checkBox2.Text = "Light";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(133, 57);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(76, 17);
+			this.checkBox3.TabIndex = 20;
+			this.checkBox3.Text = "Directional";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.AutoSize = true;
+			this.checkBox4.Location = new System.Drawing.Point(204, 57);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(47, 17);
+			this.checkBox4.TabIndex = 21;
+			this.checkBox4.Text = "Furn";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox5
+			// 
+			this.checkBox5.AutoSize = true;
+			this.checkBox5.Location = new System.Drawing.Point(133, 83);
+			this.checkBox5.Name = "checkBox5";
+			this.checkBox5.Size = new System.Drawing.Size(50, 17);
+			this.checkBox5.TabIndex = 22;
+			this.checkBox5.Text = "Point";
+			this.checkBox5.UseVisualStyleBackColor = true;
+			// 
+			// checkBox6
+			// 
+			this.checkBox6.AutoSize = true;
+			this.checkBox6.Location = new System.Drawing.Point(204, 83);
+			this.checkBox6.Name = "checkBox6";
+			this.checkBox6.Size = new System.Drawing.Size(50, 17);
+			this.checkBox6.TabIndex = 24;
+			this.checkBox6.Text = "Base";
+			this.checkBox6.UseVisualStyleBackColor = true;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(62, 107);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(65, 20);
+			this.textBox5.TabIndex = 26;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 136);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 13);
+			this.label1.TabIndex = 27;
+			this.label1.Text = "Unknown";
+			// 
+			// checkBox7
+			// 
+			this.checkBox7.AutoSize = true;
+			this.checkBox7.Enabled = false;
+			this.checkBox7.Location = new System.Drawing.Point(133, 109);
+			this.checkBox7.Name = "checkBox7";
+			this.checkBox7.Size = new System.Drawing.Size(75, 17);
+			this.checkBox7.TabIndex = 28;
+			this.checkBox7.Text = "True Point";
+			this.checkBox7.UseVisualStyleBackColor = true;
+			// 
+			// LitEntryControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.checkBox7);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.checkBox6);
+			this.Controls.Add(this.checkBox5);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this._remove_button);
+			this.Controls.Add(this._enabled_label);
+			this.Controls.Add(this._enabled_checkBox);
+			this.Controls.Add(this._colorB_textBox);
+			this.Controls.Add(this._colorG_textBox);
+			this.Controls.Add(this._positionZ_textBox);
+			this.Controls.Add(this._positionY_textBox);
+			this.Controls.Add(this._color_label);
+			this.Controls.Add(this._colorR_textBox);
+			this.Controls.Add(this._unknown2_textBox);
+			this.Controls.Add(this._flags_label);
+			this.Controls.Add(this._positionX_textBox);
+			this.Controls.Add(this._position_label);
+			this.Name = "LitEntryControl";
+			this.Size = new System.Drawing.Size(279, 185);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -185,8 +282,7 @@
 
         private System.Windows.Forms.Label _position_label;
         private System.Windows.Forms.TextBox _positionX_textBox;
-        private System.Windows.Forms.Label _unknown_label;
-        private System.Windows.Forms.TextBox _unknown1_textBox;
+        private System.Windows.Forms.Label _flags_label;
         private System.Windows.Forms.TextBox _unknown2_textBox;
         private System.Windows.Forms.TextBox _colorR_textBox;
         private System.Windows.Forms.Label _color_label;
@@ -197,5 +293,14 @@
         private System.Windows.Forms.CheckBox _enabled_checkBox;
         private System.Windows.Forms.Label _enabled_label;
         private System.Windows.Forms.Button _remove_button;
-    }
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox checkBox5;
+		private System.Windows.Forms.CheckBox checkBox6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox7;
+	}
 }
